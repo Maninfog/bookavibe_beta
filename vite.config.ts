3af9516795611f4e6e4570ae401de3798acd65d6
Maximilian Kschemien <maxi@MacBookPro.fritz.box>
2025-05-08 12:55:32 +0200

    Update base URL for new repository

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: '/bookavibestandalone/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
