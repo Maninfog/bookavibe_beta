77adf7ed49e71a231185cafb100249fc9f30090e
Maximilian Kschemien <maxi@MacBookPro.fritz.box>
2025-05-08 12:58:38 +0200

    Update base URL for bookavibe_beta repository

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: '/bookavibestandalone/',
+  base: '/bookavibe_beta/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
