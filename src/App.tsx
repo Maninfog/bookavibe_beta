93c0bb8ff6073fa3be4c47170d329e94b850889d
Maximilian Kschemien <maxi@MacBookPro.fritz.box>
2025-05-08 13:55:14 +0200

    Fix: set BrowserRouter basename and add 404.html for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/bookavibe_beta">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
